9ed7ddfa1532a57cf526a89b209355213f0f168f
mjs <me@me.com>
2021-01-06 16:01:45 +0000

    try

--- main.tf
+++ main.tf
@@ -2,12 +2,30 @@ variable "rgname" {
   type = string
 }
 
-provider "azurerm" {
-  features {}
+variable "client_id" {
+  type = string
 }
 
-provider "azuread" {
+variable "tenant_id" {
+  type = string
 }
+
+variable "client_secret" {
+  type = string
+}
+
+variable "subscription_id" {
+  type = string
+}
+
+provider "azurerm" {
+  features {}
+  client_id = var.client_id
+  tenant_id = var.tenant_id
+  client_secret = var.client_secret
+  subscription_id = var.subscription_id
+}
+
 
 resource "azurerm_resource_group" "rgname" {
   name     = var.rgname
